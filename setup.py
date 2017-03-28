6462e4369f8b815f51d12c8d2798f938555740eb
JohnDoee <johndoee@tidalstream.org>
2017-03-28 23:08:56 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(readme_path) as fp:
 
 setup(
     name='imdbparser',
-    version='1.0.3',
+    version='1.0.4',
     url='https://github.com/JohnDoee/imdbparser',
     author='John Doee',
     author_email='johndoee@tidalstream.org',
