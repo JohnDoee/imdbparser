ee8a88e3d72680d678093b2ae468e2881c7c2eeb
Anders Jensen <johndoee@tidalstream.org>
2017-12-21 21:15:45 +0100

    bumped version, removed comments

--- imdbparser/movie.py
+++ imdbparser/movie.py
@@ -141,68 +141,6 @@ class Movie(Base):
             p.name = elem.xpath('./span/text()')[0]
             self.actors.append(p)
 
-
-        # for elem in self.trees[0].xpath("//div[@class='info']"):
-        #     info_text = elem.xpath(".//h5")[0].text
-        #     if not info_text:
-        #         continue
-        #     info_content = elem.xpath(".//div[@class='info-content']")[0]
-
-        #     if info_text == 'Release Date:':
-        #         self.release_date = info_content.text.strip()
-        #     elif info_text == 'Genre:':
-        #         self.genres = [x.text for x in info_content.xpath('.//a') if '/Genres/' in x.attrib['href']]
-        #     elif info_text == 'Tagline:':
-        #         self.description = info_content.text.strip()
-        #     elif info_text == 'Plot:':
-        #         self.plot = info_content.text.strip(' |\n')
-        #     elif info_text == 'Plot Keywords:':
-        #         self.plot_keywords = [x.text for x in info_content.xpath(".//a") if '/keyword/' in x.attrib['href']]
-        #     elif info_text == 'Also Known As:':
-        #         for elem in info_content.xpath("./text()"):
-        #             title = elem.strip().split('"')
-        #             if len(title) > 2 and len(title[0]) == 0:
-        #                 self.alternative_titles.append('"'.join(title[1:-1]))
-        #     elif info_text == 'Runtime:':
-        #         runtimes = re.findall('(\d+) min', info_content.text)
-        #         if runtimes:
-        #             self.duration = int(runtimes[0])
-        #     elif info_text == 'Country:':
-        #         self.countries = [x.text for x in info_content.xpath(".//a") if '/country/' in x.attrib['href']]
-        #     elif info_text == 'Language:':
-        #         self.languages = [x.text for x in info_content.xpath(".//a") if '/language/' in x.attrib['href']]
-        #     elif info_text == 'Company:':
-        #         self.companies = [x.text for x in info_content.xpath(".//a") if '/company/' in x.attrib['href']]
-        #     elif info_text.startswith('Director'):
-        #         for elem in info_content.xpath(".//a"):
-        #             if '/name/' not in elem.attrib['href']:
-        #                 continue
-        #             p = Person(re.findall('/nm(\d+)', elem.attrib['href'])[0], self.imdb)
-        #             p.name = elem.text
-        #             self.directors.append(p)
-        #     elif info_text.startswith('Writer'):
-        #         for elem in info_content.xpath(".//a"):
-        #             if '/name/' not in elem.attrib['href']:
-        #                 continue
-        #             p = Person(re.findall('/nm(\d+)', elem.attrib['href'])[0], self.imdb)
-        #             p.name = elem.text
-        #             self.writers.append(p)
-
-        # if not self.release_date:
-        #     release_dates = [x.strip() for x in self.trees[1].xpath("//h4[text()='Release Date:']/../text()") if x.strip()]
-        #     if release_dates:
-        #         self.release_date = release_dates[0]
-
-        # for elem in self.trees[0].xpath("//table[@class='cast']/tr"):
-        #     elem = elem.xpath(".//td[@class='nm']/a")
-        #     if not elem:
-        #         break
-        #     elem = elem[0]
-
-        #     p = Person(re.findall('/nm(\d+)', elem.attrib['href'])[0], self.imdb)
-        #     p.name = elem.text
-        #     self.actors.append(p)
-
         self.alternative_titles = list(set(self.alternative_titles))
 
     def __repr__(self):
